151fd55ab7b1515f8a4e41e521ab77c791b31a20
yasmimmaciel04-art <yasmim.maciel04@escola.pr.gov.br>
2026-09-09 10:28:16 -0300

    09/09

--- main.js
+++ main.js
@@ -2,12 +2,9 @@ const botoes= document. querySelectorall(".botao");
     for(let i=0;i<botoes. lenght; i++){
         botoes[i].onclick= function(){
             botoes[i].classlist.add("ativo");
-            botoes[i]. 
+             botoes[i]. classlist.add("ativo");
         }
     }
         
-      
-
-   botoes[i].classlist.add("ativo");
-      botoes[i].classlist.add("ativo");
+    
     
